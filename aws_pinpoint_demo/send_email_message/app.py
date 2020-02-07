44ca6779c1578403bcf9334842bd18e5639b79c8
JamesFormosa <formosa@amazon.com>
2020-02-07 07:13:20 +0000

    completed kinesis-pinpoint integration 	modified:   packaged.yaml 	new file:   process_event_stream/app.py 	modified:   send_email_message/app.py 	modified:   template.yaml

--- aws_pinpoint_demo/send_email_message/app.py
+++ aws_pinpoint_demo/send_email_message/app.py
@@ -1,23 +1,24 @@
 import boto3
 import json
+import os
 
 def lambda_handler(event, context):
     print(event)
     s3 = boto3.client('s3')
-    s3.download_file('change-pinpoint-templates', event['message_template'], '/tmp/pinpoint_template.html')
+    s3.download_file(os.environ['TEMPLATE_BUCKET'], event['message_template'], '/tmp/pinpoint_template.html')
     with open ("/tmp/pinpoint_template.html", "r") as myfile:
         data=myfile.readlines()
     print(data[0])
     pinpoint = boto3.client('pinpoint')
     response = pinpoint.send_messages(
-            ApplicationId='c83228417e1e4619a61fabcfaef18b1a',
+            ApplicationId=os.environ['APPLICATION_ID'],
             MessageRequest={
                 'Context': {
                     'campaign_id': event['campaign_id'],
                     'user_id': event['user_id']
                 },
                 'Addresses': {
-                    'formosa@amazon.com': {
+                        event['email_address']: {
                         'ChannelType': 'EMAIL'
                     }
                 },
